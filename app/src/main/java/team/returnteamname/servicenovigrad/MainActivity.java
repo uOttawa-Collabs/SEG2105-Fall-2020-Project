fa4b3d96bbf5da190490ccdf0410fcfbf76d6308
wxx9248 <wxx9248@qq.com>
2020-10-13 10:54:10 -0400

    Add firebase support

--- app/src/main/java/team/returnteamname/servicenovigrad/MainActivity.java
+++ app/src/main/java/team/returnteamname/servicenovigrad/MainActivity.java
@@ -1,8 +1,19 @@
 package team.returnteamname.servicenovigrad;
 
 import android.os.Bundle;
+import android.renderscript.Sampler;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +22,23 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = database.getReference("testKey");
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot)
+            {
+                textView.setText(Objects.requireNonNull(snapshot.getValue()).toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error)
+            {
+                textView.setText("Request cancelled");
+            }
+        });
     }
 }
