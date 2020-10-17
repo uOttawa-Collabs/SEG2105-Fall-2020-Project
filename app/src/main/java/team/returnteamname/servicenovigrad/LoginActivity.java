168a36123b6e8eab5e6a93e31171d3367a6d2914
wxx9248 <wxx9248@qq.com>
2020-10-17 06:21:43 -0400

    UI & activities fixed Hardcode string to be extracted later

--- app/src/main/java/team/returnteamname/servicenovigrad/LoginActivity.java
+++ app/src/main/java/team/returnteamname/servicenovigrad/LoginActivity.java
@@ -1,16 +1,15 @@
 package team.returnteamname.servicenovigrad;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class LoginPage extends AppCompatActivity
-{
+import androidx.appcompat.app.AppCompatActivity;
 
+public class LoginActivity extends AppCompatActivity
+{
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_page);
+        setContentView(R.layout.activity_login);
     }
 }
--- app/src/main/java/team/returnteamname/servicenovigrad/RegisterActivity.java
+++ app/src/main/java/team/returnteamname/servicenovigrad/RegisterActivity.java
@@ -1,14 +1,15 @@
 package team.returnteamname.servicenovigrad;
+
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity
+public class RegisterActivity extends AppCompatActivity
 {
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
     }
 }
