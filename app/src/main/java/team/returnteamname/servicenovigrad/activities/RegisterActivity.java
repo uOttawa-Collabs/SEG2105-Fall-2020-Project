65ad6336de420e9442d70e1b73e0fd7d7e714705
Jinnnn-guo <64765376+Jinnnn-guo@users.noreply.github.com>
2020-10-18 12:47:00 -0400

    Update RegisterActivity.java
    stored values of edit text

--- app/src/main/java/team/returnteamname/servicenovigrad/activities/RegisterActivity.java
+++ app/src/main/java/team/returnteamname/servicenovigrad/activities/RegisterActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import team.returnteamname.servicenovigrad.R;
@@ -26,6 +27,22 @@ public class RegisterActivity extends Activity
                                                                              R.array.rolesRegister, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
-        
+
+
+        EditText[] editTextValues = new EditText[6];
+
+        editTextValues[0] = (EditText) findViewById(R.id.editTextFirstName);
+        editTextValues[1] = (EditText) findViewById(R.id.editTextLastName);
+        editTextValues[2]  = (EditText) findViewById(R.id.editTextEmail);
+        editTextValues[3] = (EditText) findViewById(R.id.editTextUsername);
+        editTextValues[4] = (EditText) findViewById(R.id.editTextPassword);
+        editTextValues[5] = (EditText) findViewById(R.id.editTextPasswordConfirm);
+
+        String   firstName  = editTextValues[0].getText().toString();
+        String   lastName  = editTextValues[1] .getText().toString();
+        String   userEmail  = editTextValues[2].getText().toString();
+        String   username  = editTextValues[3].getText().toString();
+        String   userPassword  = editTextValues[4].getText().toString();
+        String   userPassword2  = editTextValues[5].getText().toString();
     }
 }
