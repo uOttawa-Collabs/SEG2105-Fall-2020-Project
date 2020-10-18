4f10960535a266c474c4421894bb00ccac754a00
Jinnnn-guo <64765376+Jinnnn-guo@users.noreply.github.com>
2020-10-18 12:32:21 -0400

    spinner content added

--- app/src/main/java/team/returnteamname/servicenovigrad/activities/RegisterActivity.java
+++ app/src/main/java/team/returnteamname/servicenovigrad/activities/RegisterActivity.java
@@ -3,6 +3,8 @@ package team.returnteamname.servicenovigrad.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import team.returnteamname.servicenovigrad.R;
 
@@ -13,9 +15,17 @@ public class RegisterActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
         // onclick event
         findViewById(R.id.buttonRegisterSubmit).setOnClickListener(
             v -> startActivityForResult(new Intent(getApplicationContext(), WelcomeActivity.class),
                                         0));
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerRoleSelector);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                                                                             R.array.rolesRegister, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        
     }
 }
