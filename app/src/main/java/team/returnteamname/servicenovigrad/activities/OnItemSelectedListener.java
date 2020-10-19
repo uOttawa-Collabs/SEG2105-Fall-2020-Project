7ae65eae8fa4db1ad2156e4bdd435cf5057c821b
Jinnnn-guo <64765376+Jinnnn-guo@users.noreply.github.com>
2020-10-18 21:14:48 -0400

    Update OnItemSelectedListener.java

--- app/src/main/java/team/returnteamname/servicenovigrad/activities/OnItemSelectedListener.java
+++ app/src/main/java/team/returnteamname/servicenovigrad/activities/OnItemSelectedListener.java
@@ -8,12 +8,14 @@ import team.returnteamname.servicenovigrad.account.AccountManager;
 
 public class OnItemSelectedListener extends Activity implements AdapterView.OnItemSelectedListener
 {
+
+    @Override
     public void onItemSelected(AdapterView<?> parent, View view,
                                int pos, long id) {
         if (AccountManager.getInstance().isInitialized()){}
         parent.getItemAtPosition(pos).toString();
     }
-
+    @Override
     public void onNothingSelected(AdapterView<?> parent) {
         // Another interface callback
     }
