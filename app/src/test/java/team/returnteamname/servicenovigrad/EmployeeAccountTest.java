36a4e7d43b11b3e72c525a23c028a36c210c728d
Feier Zhang <fayerzhang0301@yahoo.com>
2020-11-22 13:54:19 -0500

    Upload EmployeeAccountTest

--- app/src/test/java/team/returnteamname/servicenovigrad/EmployeeAccountTest.java
+++ app/src/test/java/team/returnteamname/servicenovigrad/EmployeeAccountTest.java
@@ -18,4 +18,34 @@ public class EmployeeAccountTest
         assertEquals("Check the first name of the employee", "Ben", aUser.getFirstName());
     }
 
+    @Test
+    public void checkEmployeeLastName(){
+        EmployeeAccount aUser = new EmployeeAccount("benEmployee", "123456", "Employee", "Ben", "Alex", "benalex@gmail.com" );
+        assertEquals("Check the last name of the employee", "Alex", aUser.getLastName());
+    }
+
+    @Test
+    public void checkEmployeeUserName(){
+        EmployeeAccount aUser = new EmployeeAccount("benEmployee", "123456", "Employee", "Ben", "Alex", "benalex@gmail.com" );
+        assertEquals("Check the username of the employee", "benEmployee", aUser.getUsername());
+    }
+
+    @Test
+    public void checkPassword(){
+        EmployeeAccount aUser = new EmployeeAccount("benEmployee", "123456", "Employee", "Ben", "Alex", "benalex@gmail.com" );
+        assertEquals("Check the password of the employee", "123456", aUser.getPassword());
+    }
+
+    @Test
+    public void checkRole(){
+        EmployeeAccount aUser = new EmployeeAccount("benEmployee", "123456", "Employee", "Ben", "Alex", "benalex@gmail.com" );
+        assertEquals("Check the role of the employee", "Employee", aUser.getRole());
+    }
+
+    @Test
+    public void checkEmail(){
+        EmployeeAccount aUser = new EmployeeAccount("benEmployee", "123456", "Employee", "Ben", "Alex", "benalex@gmail.com" );
+        assertEquals("Check the email of the employee", "benalex@gmail.com", aUser.getEmail());
+    }
+
 }
